1e94e0a665e6e38e082df8a7f76f22adc7585f50
Чащевая Яна <chashchevayana@mail.ru>
2026-06-15 21:35:49 +0300

    add multi-module project structure for stats service

--- stats/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,10 +1,11 @@
-package dto.src.main.java.ru.practicum.dto;
+package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @NoArgsConstructor
--- stats/dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
+++ stats/dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -1,9 +1,11 @@
-package dto.src.main.java.ru.practicum.dto;
+package ru.practicum.dto;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @NoArgsConstructor
