a87b254aee3e16cbd44a339bf153d76f32243184
Чащевая Яна <chashchevayana@mail.ru>
2026-06-17 14:55:50 +0300

    Fix PR tests

--- stats/server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats/server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -22,7 +22,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM EndpointHit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.app, h.uri" +
+            "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStats> findStatsUnique(@Param("start") LocalDateTime start,
                                     @Param("end") LocalDateTime end);
@@ -31,7 +31,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
             "FROM EndpointHit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
             "AND h.uri IN :uri " +
-            "GROUP BY h.app, h.uri" +
+            "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(h.uri) DESC")
     List<ViewStats> findStatsWithUris(@Param("start") LocalDateTime start,
                                       @Param("end") LocalDateTime end,
@@ -41,7 +41,7 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
             "FROM EndpointHit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
             "AND h.uri IN :uri " +
-            "GROUP BY h.app, h.uri" +
+            "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStats> findStatsUniqueWithUris(@Param("start") LocalDateTime start,
                                             @Param("end") LocalDateTime end,
