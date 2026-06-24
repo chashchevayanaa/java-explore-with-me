e6937ba0ab3f3df3fc800f619abd4c70938733b4
Чащевая Яна <chashchevayana@mail.ru>
2026-06-24 16:06:33 +0300

    Fix PR tests

--- stats/client/src/main/java/ru/practicum/StatsClientImpl.java
+++ stats/client/src/main/java/ru/practicum/StatsClientImpl.java
@@ -2,7 +2,6 @@ package ru.practicum;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
