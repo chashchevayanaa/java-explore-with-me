3f9ee25a073c0410d73f8b12cc5223530b4d6a6d
Чащевая Яна <chashchevayana@mail.ru>
2026-06-24 16:15:57 +0300

    Fix checkstyle

--- ewm/src/main/java/ru/practicum/repository/EventRepository.java
+++ ewm/src/main/java/ru/practicum/repository/EventRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.model.Event;
 
 import java.util.Optional;
